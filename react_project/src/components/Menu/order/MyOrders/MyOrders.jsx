f20d14d77ef7f02aa022ab380c0d125d36315deb
ahmedehab311 <ahmedehab.aa47@gmail.com>
2024-08-28 13:06:00 +0300

    update

--- react_project/src/components/Menu/order/MyOrders/MyOrders.jsx
+++ react_project/src/components/Menu/order/MyOrders/MyOrders.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Stack, Typography, Card, CardContent, Button } from "@mui/material";
+import { Stack, Typography, Card, CardContent, Box } from "@mui/material";
 import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
 import DoneOutlinedIcon from "@mui/icons-material/DoneOutlined";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
-// import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
 import img from "./meal.jpg";
 
 function MyOrders() {
@@ -185,18 +184,44 @@ function MyOrders() {
                     direction={"row"}
                     alignItems={"center"}
                   >
-                    <Stack sx={{ width: "100px", height: "50px" }}>
-                      <img src={img} alt="logo" />
+                    <Stack
+                      sx={{
+                        width: "180px",
+                        height: "90px",
+                        padding: "8px",
+                        borderRadius: ".5rem",
+                        border: "1px solid #f8f9fa",
+                        transition: "background-color 0.9s ease",
+                        cursor: "pointer",
+                        mr: "1.5rem",
+                        boxShadow:
+                          "0 .125rem .25rem rgba(0,0,0,.075)!important",
+                        "&:hover": {
+                          backgroundColor: "#f8f9fa",
+                        },
+                      }}
+                    >
+                      <Box
+                        component="img"
+                        src={img}
+                        alt="logo"
+                        sx={{
+                          width: "100%",
+                          height: "100%",
+                          borderRadius: ".5rem",
+                          objectFit: "contain",
+                        }}
+                      />
                     </Stack>
-
                     <Stack>
                       <Stack direction={"row"} alignItems={"center"}>
                         <Typography sx={{ fontSize: "2rem", color: "#000 " }}>
-                          Order{" "}
+                          Order
                         </Typography>
                         <Typography
                           sx={{
                             fontSize: "2rem",
+                            fontWeight: "600",
                             ml: ".5rem",
                             color: "#17a2b8!important",
                           }}
@@ -211,13 +236,16 @@ function MyOrders() {
                           width: "fit-content",
                           mt: "1.3rem",
                           p: ".8rem 2rem",
-                          fontSize: "1.3rem",
+                          fontSize: "1.6rem",
+                          fontWeight: "bold",
                           cursor: "pointer",
-                          transition: "background-color 0.8s ease",
+                          transition:
+                            "background-color 0.6s ease, color 0.6s ease, border-color 0.6s ease",
+                          borderRadius: ".8rem",
                           "&:hover": {
                             backgroundImage:
                               "linear-gradient(135deg, #cea076 0%, #bd946d 100%)",
-                            color: "white",
+                            // color: "inh",
                             borderColor: "#cea076",
                           },
                         }}
@@ -227,20 +255,59 @@ function MyOrders() {
                     </Stack>
                   </Stack>
                   <Stack sx={{ ml: "auto" }}>
-                    <Typography variant="h6">progress</Typography>
+                    <Typography
+                      variant="h6"
+                      sx={{
+                        background:
+                          "linear-gradient(45deg, #613233 0%, #663435 100%)",
+                        color: "#fff",
+                        textAlign: "center",
+                        width: "fit-content",
+                        mt: "1.3rem",
+                        p: ".5rem 6rem",
+                        fontSize: "1.3rem",
+                        fontWeight: "500",
+                        borderRadius: ".8rem",
+                        mb: ".9rem",
+                      }}
+                    >
+                      progress
+                    </Typography>
                     <Stack
                       variant="body2"
                       alignItems={"center"}
                       direction={"row"}
                     >
-                      <AccessTimeOutlinedIcon
-                        sx={{ fontSize: "1.8rem", mr: ".4rem" }}
-                      />{" "}
-                      {order.schedule}
+                      <AccessTimeOutlinedIcon sx={{ mr: ".4rem" }} />
+                      <Typography
+                        sx={{
+                          fontSize: "1.5rem",
+                          color: "#424242",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        {order.created_at}
+                      </Typography>
                     </Stack>
                     <Stack>
-                      <Typography>Total Payment</Typography>
-                      <Typography>657.78 EGP</Typography>
+                      <Typography
+                        sx={{
+                          fontSize: "1.5rem",
+                          color: "#6c757d!important",
+                          fontWeight: "600",
+                        }}
+                      >
+                        Total Payment
+                      </Typography>
+                      <Typography
+                        sx={{
+                          fontSize: "1.5rem",
+                          color: "#343a40!important",
+                          fontWeight: "bold",
+                        }}
+                      >
+                        657.78 EGP
+                      </Typography>
                     </Stack>
                   </Stack>
                 </Stack>
