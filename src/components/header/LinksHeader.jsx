950119843b08191c252f6c5a7bcb1c3bb1c5e895
ahmedehab311 <ahmedehab.aa47@gmail.com>
2024-09-24 17:35:10 +0300

    Y

--- src/components/header/LinksHeader.jsx
+++ src/components/header/LinksHeader.jsx
@@ -110,7 +110,7 @@ function LinksHeader({ handleNavigation }) {
         onClose={handleClose}
       >
         <MenuItem onClick={() => changeLanguage('en')}>English</MenuItem>
-        <MenuItem onClick={() => changeLanguage('ar')}>العربية</MenuItem>
+        {/* <MenuItem onClick={() => changeLanguage('ar')}>العربية</MenuItem> */}
       </Menu>
       <Link href="#menu">
         <Typography
